7304929ea259a0630ca1a4cb9eb095203f026228
Guilherme Haynes Howe <zerossb@gmail.com>
2021-08-15 20:30:30 -0300

    Add terraform version and aws provider version

--- aws_s3_bucket01/main.tf
+++ aws_s3_bucket01/main.tf
@@ -1,14 +1,24 @@
+terraform {
+  required_version = "1.0.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
   profile = "terraform-teste"
 }
 
 resource "aws_s3_bucket" "mu-test-bucket" {
   bucket = "my-tf-test-bucket-dohaynes"
-  acl = "private"
+  acl    = "private"
 
   tags = {
-    "Name" = "My Bucket"
+    "Name"        = "My Bucket"
     "Environment" = "develop"
   }
 }
